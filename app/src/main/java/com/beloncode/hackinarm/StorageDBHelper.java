93c207570929e7998c2238beff26cd3f7d451de3
Gabriel Correia <gabrielcorreia01012020@hotmail.com>
2023-01-08 13:47:05 -0300

    [v0.1] - Fixing an error while tried to retrieve a writable DB

--- app/src/main/java/com/beloncode/hackinarm/StorageDBHelper.java
+++ app/src/main/java/com/beloncode/hackinarm/StorageDBHelper.java
@@ -10,7 +10,7 @@ public class StorageDBHelper extends SQLiteOpenHelper {
 
     public static final int DATABASE_VERSION = 1;
     public static final String DATABASE_FILEPATH = AppDBContract.APP_DB_FILEPATH;
-    
+
     public StorageDBHelper(Context context) {
         super(context, DATABASE_FILEPATH, null, DATABASE_VERSION);
     }
@@ -28,10 +28,9 @@ public class StorageDBHelper extends SQLiteOpenHelper {
         dbInitialValues.put(dbPath, ExternalDBContract.EXT_DATABASE_FILEPATH);
 
         final String tableName = AppDBContract.StorageContent.TABLE_STORAGE_NAME;
-        SQLiteDatabase dbWritable = getWritableDatabase();
-        dbWritable.insert(tableName, null, dbInitialValues);
 
-        dbWritable.close();
+        db.insert(tableName, null, dbInitialValues);
+
     }
 
     @Override
